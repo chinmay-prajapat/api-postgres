95474b9d69e02b17a51ea063db67a788b92805c4
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-10-29 15:17:22 +0530

    promise added

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const app = require("./src/app");
 const pool = require("./src/pool");
-pool.connect({
-  host: "localhost",
-  port: 5432,
-  database: "socialnetwork",
-  user: "postgres",
-  password: "7894",
-});
-app().listen(3005, () => {
-  console.log("Listening on port 3005");
-});
+pool
+  .connect({
+    host: "localhost",
+    port: 5432,
+    database: "socialnetwork",
+    user: "postgres",
+    password: "7894",
+  })
+  .then(() => {
+    app().listen(3005, () => {
+      console.log("Listening on port 3005");
+    });
+  })
+  .catch((err) => console.log(err));
